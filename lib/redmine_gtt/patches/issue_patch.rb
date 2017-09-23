df606c1e636bf5015dd1b553c66e9c65d93ef1bb
Jens Kraemer <jk@jkraemer.net>
2017-09-23 10:53:35 +0800

    refactoring - removes triplicate wkb/json conversion code #50

--- lib/redmine_gtt/patches/issue_patch.rb
+++ lib/redmine_gtt/patches/issue_patch.rb
@@ -6,6 +6,7 @@ module RedmineGtt
       def self.apply
         unless Issue < self
           Issue.prepend self
+          Issue.prepend GeojsonAttribute
           Issue.class_eval do
             safe_attributes "geom",
               if: ->(issue, user){ user.allowed_to?(:edit_issues, issue.project)}
@@ -13,34 +14,6 @@ module RedmineGtt
         end
       end
 
-      def geojson
-        unless self.geom.nil?
-          factory = RGeo::GeoJSON::EntityFactory.instance
-          wkb = RGeo::WKRep::WKBParser.new().parse(self.geom)
-          RGeo::GeoJSON.encode factory.feature(wkb, self.id, self.as_json)
-        else
-          nil
-        end
-      end
-
-      def geom=(g)
-        # Turn geometry attribute into WKB for database use
-        if (g.present?)
-          begin
-            geojson = JSON.parse(g)
-            feature = RGeo::GeoJSON.decode(geojson, json_parser: :json)
-            wkb = RGeo::WKRep::WKBGenerator.new(
-              :hex_format => true
-            )
-            self[:geom] = wkb.generate(feature.geometry)
-          rescue
-            # The Gemetry is likely to be already in WKB format
-            self[:geom] = g
-          end
-        else
-          self[:geom] = nil
-        end
-      end
     end
 
   end
