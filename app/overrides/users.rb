ff892ef16f8638808f527cdef39c90bda1739099
Jens Kraemer <jk@jkraemer.net>
2017-09-23 12:53:51 +0800

    refactoring: move users/edit map to view hook #50

--- app/overrides/users.rb
+++ app/overrides/users.rb
@@ -1,12 +1,4 @@
 Deface::Override.new(
-  :virtual_path => "users/_form",
-  :name => "deface_view_users_form_map",
-  :insert_bottom => "div.splitcontentleft",
-  :partial => "users/form/map"
-)
-
-
-Deface::Override.new(
   :virtual_path => "users/show",
   :name => "deface_view_users_show_other_formats",
   :insert_bottom => "div.splitcontentleft",
