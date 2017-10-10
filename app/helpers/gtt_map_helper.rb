24bdaeb73563090ab5ff390eea77635e6f16c0fb
Jens Kraemer <jk@jkraemer.net>
2017-10-10 11:13:25 +0800

    only initialize the current map
    - issues#show would whod a double map otherwise (since the helper is called
      twice - once for show, once for the (hidden) form

--- app/helpers/gtt_map_helper.rb
+++ app/helpers/gtt_map_helper.rb
@@ -34,7 +34,7 @@ module GttMapHelper
       content_tag(:div, "", data: data, id: uid, class: 'ol-map'),
       javascript_tag("
         $(document).ready(function(){
-          $('div.ol-map').each(function(idx) {
+          $('##{uid}').each(function(idx) {
             App.init({
               target: this
             });
