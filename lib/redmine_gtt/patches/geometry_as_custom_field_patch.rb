9703489dfaf0842b9c85014b4959b568d7c6b3c2
Jens Kraemer <jk@jkraemer.net>
2017-10-10 10:00:51 +0800

    render geometry (WKT) in issue PDF #76

---
+++ lib/redmine_gtt/patches/geometry_as_custom_field_patch.rb
@@ -0,0 +1,46 @@
+module RedmineGtt
+  module Patches
+    # this module extends an issue so it's visible_custom_field_values include
+    # an additional value representing the geometry.
+    #
+    # This hack allows the geometry being rendered in the issue PDF variant
+    # without having to core-patch Redmine's PDF rendering code.
+    module GeometryAsCustomFieldPatch
+
+      class GeometryFieldFormat
+        def formatted_custom_value(view, object, html)
+          object.value
+        end
+      end
+
+      class GeometryCustomField
+        attr_reader :format, :name
+        def initialize
+          @format = GeometryFieldFormat.new
+          @name = I18n.t :field_geom
+        end
+        def full_width_layout?
+          true
+        end
+      end
+
+      class GeomtryCustomFieldValue < ::CustomFieldValue
+        attr_reader :custom_field
+
+        def initialize(customized)
+          @customized = customized
+          @custom_field = GeometryCustomField.new
+        end
+        def value
+          @customized.geom
+        end
+      end
+
+      def visible_custom_field_values
+        super.tap do |values|
+          values << GeomtryCustomFieldValue.new(self)
+        end
+      end
+    end
+  end
+end
