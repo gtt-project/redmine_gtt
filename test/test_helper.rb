a73bb8dcf3155e29c419c52756a2db652dda65cf
Jens Kraemer <jk@jkraemer.net>
2018-03-19 11:55:58 +0800

    unit test fixes
    - my tests were barking about the wkb having the wrong srid. using json works.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
 class GttTest < ActiveSupport::TestCase
 
+  def test_geojson
+    {'type'=>'Feature','geometry'=>{ 'type'=>'Polygon','coordinates'=> [[[135.22073422241215,34.70569060003112],[135.30227337646488,34.6990600142143],[135.3000417785645,34.670969984370885],[135.25283489990238,34.676052303889435],[135.1942125396729,34.67668404351015],[135.22073422241215,34.70569060003112]]]}}.to_json
+  end
+
   def test_geom
     RedmineGtt::Conversions::WkbToGeom.("01030000000100000006000000C84B374110E76040381DD011545A4140C84B3739ACE96040F07E6DCC7A594140C84B37F199E960403CBC2D58E2554140C84B373917E8604098CBC3E188564140C84B37FD36E66040F24C2E959D564140C84B374110E76040381DD011545A4140")
   end
 
   def assert_geojson(json)
+    json = JSON.parse json if json.is_a?(String)
     assert_equal 'Feature', json['type']
     assert geom = json['geometry']
     assert_equal 'Polygon', geom['type']
