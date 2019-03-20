3f3b1408c55c9e2357c2340b16b42001266bb9c3
Thibault <933kss@gmail.com>
2019-03-20 12:29:09 +0900

    add before_update callback

--- lib/redmine_gtt/patches/issue_patch.rb
+++ lib/redmine_gtt/patches/issue_patch.rb
@@ -15,7 +15,7 @@ module RedmineGtt
                 perm = issue.new_record? ? :add_issues : :edit_issues
                 user.allowed_to? perm, issue.project
               }
-
+            before_update :ignore_small_geom_change, if: :geom_changed?
           end
         end
       end
@@ -24,6 +24,19 @@ module RedmineGtt
         json = as_geojson
         GttMap.new json: json, layers: project.gtt_tile_sources.sorted,
           bounds: (new_record? ? project.map.json : json)
+      end
+
+      # Check if geometry change aren't small and ignore it
+      # (i.e. [140.1250590699026,35.6097256061325] vs [140.1250590699026,35.60972560613251])
+
+      def ignore_small_geom_change
+        unless geom_change[0].nil?
+          if geom_change[0].as_json.fetch("type") == geom_change[1].as_json.fetch("type")
+            old_value = geom_change[0].as_json.fetch("coordinates")
+            new_value = geom_change[1].as_json.fetch("coordinates")
+            self.geom = geom_change[0] if new_value.zip(old_value).map { |a, b| (a-b).abs }.map {|x| x < 0.00001}.all?
+          end
+        end
       end
 
       module ClassMethods
