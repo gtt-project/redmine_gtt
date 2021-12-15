71e6f5fc617e0e381cb28a5f90824cec7b0d75c9
Ko Nagase <nagase@georepublic.co.jp>
2021-12-15 09:15:59 +0900

    Adjusted deface override original value and position

--- app/overrides/projects.rb
+++ app/overrides/projects.rb
@@ -2,6 +2,7 @@ Deface::Override.new(
   :virtual_path => "projects/show",
   :name => "deface_view_projects_show_map",
   :insert_bottom => "div.splitcontentright",
+  :original => 'b939fb5ea208476399dbfb4b253dcff0ab1ace91',
   :partial => "projects/show/map"
 )
 
@@ -9,5 +10,6 @@ Deface::Override.new(
   :virtual_path => "projects/show",
   :name => "deface_view_projects_show_other_formats",
   :insert_bottom => "div.splitcontentright",
+  :original => '868cdb34ed1c52af47076776295dcba1311914f9',
   :partial => "projects/show/other_formats"
 )
