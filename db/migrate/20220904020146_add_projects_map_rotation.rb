4c708b7355143f804ed3ab0afe167c86d4620f82
Daniel Kastl <daniel@georepublic.de>
2023-02-14 23:53:13 +0900

    Fixes CI migration version issue
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- db/migrate/20220904020146_add_projects_map_rotation.rb
+++ db/migrate/20220904020146_add_projects_map_rotation.rb
@@ -1,4 +1,4 @@
-class AddProjectsMapRotation < ActiveRecord::Migration[6.1]
+class AddProjectsMapRotation < ActiveRecord::Migration[5.2]
   def self.up
     add_column :projects, :map_rotation, :integer, default: 0
   end
