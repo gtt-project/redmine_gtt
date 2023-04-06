d29e38de60173f0b009f33338a1ba9590f7eb5f9
Daniel Kastl <daniel@georepublic.de>
2023-04-06 11:14:50 +0900

    Better readablity and comments
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- Gemfile
+++ Gemfile
@@ -1,10 +1,18 @@
 source 'https://rubygems.org'
 
+# Define gem versions with environment variables or default versions
+gem_versions = {
+  pg: ENV['GEM_PG_VERSION'] || '1.2.2',
+  rgeo: ENV['GEM_RGEO_VERSION'] || '2.4.0',
+  rgeo_activerecord: ENV['GEM_RGEO_ACTIVERECORD_VERSION'] || '7.0.1',
+  activerecord_postgis_adapter: ENV['GEM_ACTIVERECORD_POSTGIS_ADAPTER_VERSION'] || '7.1.1'
+}
+
 gem 'deface'
 gem 'immutable-struct'
-gem "rgeo", "~> 2.4.0"
+gem "rgeo", "~> #{gem_versions[:rgeo]}"
 gem "rgeo-geojson"
-gem "pg", (ENV['GEM_PG_VERSION'] ? "~> #{ENV['GEM_PG_VERSION']}" : "~> 1.2.2") # make sure we use a version compatible with AR
-gem "rgeo-activerecord", (ENV['GEM_RGEO_ACTIVERECORD_VERSION'] ? "~> #{ENV['GEM_RGEO_ACTIVERECORD_VERSION']}" : "~> 7.0.1") # same as above
-gem 'activerecord-postgis-adapter', (ENV['GEM_ACTIVERECORD_POSTGIS_ADAPTER_VERSION'] ? "~> #{ENV['GEM_ACTIVERECORD_POSTGIS_ADAPTER_VERSION']}" : "~> 7.1.1") # same as above
+gem "pg", "~> #{gem_versions[:pg]}"
+gem "rgeo-activerecord", "~> #{gem_versions[:rgeo_activerecord]}"
+gem 'activerecord-postgis-adapter', "~> #{gem_versions[:activerecord_postgis_adapter]}"
 gem 'rails-controller-testing' # This gem brings back assigns to your controller tests as well as assert_template to both controller and integration tests.
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,53 @@
-const path = require('path')
+const path = require('path');
+
+// Define loaders
+// Loaders for processing Sass files
+const sassLoaders = [
+  'style-loader', // Creates `style` nodes from JS strings
+  'css-loader', // Translates CSS into CommonJS
+  'sass-loader', // Compiles Sass to CSS
+];
+
+// Loaders for processing CSS files
+const cssLoaders = ['style-loader', 'css-loader'];
+
+// Loaders for processing image files
+const imageLoaders = {
+  test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  type: 'asset/resource',
+};
+
+// Loaders for processing font files
+const fontLoaders = {
+  test: /\.(woff|woff2|eot|ttf|otf)$/i,
+  type: 'asset/resource',
+};
+
+// Loaders for processing TypeScript files
+const tsLoaders = {
+  test: /\.ts$/i,
+  use: 'ts-loader',
+};
 
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  mode: 'production', // Set build mode to production
+  entry: path.join(__dirname, 'src', 'index.ts'), // Specify entry point
   module: {
     rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.ts$/i,
-        use: 'ts-loader',
-      },
+      // Apply loaders
+      { test: /\.s[ac]ss$/i, use: sassLoaders },
+      { test: /\.css$/i, use: cssLoaders },
+      imageLoaders,
+      fontLoaders,
+      tsLoaders,
     ],
   },
-  devtool: false,
+  devtool: false, // Disable source maps
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'], // Specify file extensions to resolve
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'assets', 'javascripts')
-  }
-}
+    filename: 'main.js', // Set output filename
+    path: path.join(__dirname, 'assets', 'javascripts'), // Set output directory
+  },
+};
