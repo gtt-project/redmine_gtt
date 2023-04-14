79716d729344a8c89b11aef9490e5d87fd948cdd
Daniel Kastl <daniel@georepublic.de>
2023-04-14 20:34:18 +0900

    Fixes source_options
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- test/unit/create_map_layer_test.rb
+++ test/unit/create_map_layer_test.rb
@@ -6,7 +6,7 @@ class CreateMapLayerTest < ActiveSupport::TestCase
     r = RedmineGtt::Actions::CreateMapLayer.(layer: 'Tile', name: 'test', source_options_string: '{ "url": "https://example.com" }')
     assert r.map_layer_created?
     assert_equal 'test', r.map_layer.name
-    assert_equal 'https://example.com', r.map_layer.options['url']
+    assert_equal 'https://example.com', r.map_layer.source_options['url']
   end
 
   test 'should validate json' do
