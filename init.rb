c134e25e47551f3e6036b850ae87e97058e57bb8
Daniel Kastl <daniel@georepublic.de>
2023-06-19 12:28:20 +0900

    Reverts version updates due to CI issue
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- init.rb
+++ init.rb
@@ -8,9 +8,9 @@ Redmine::Plugin.register :redmine_gtt do
   author_url 'https://github.com/georepublic'
   url 'https://github.com/gtt-project/redmine_gtt'
   description 'Adds location-based task management and maps'
-  version '5.0.0'
+  version '4.3.1'
 
-  requires_redmine :version_or_higher => '5.0.0'
+  requires_redmine :version_or_higher => '4.2.0'
 
   project_module :gtt do
     permission :manage_gtt_settings, {
