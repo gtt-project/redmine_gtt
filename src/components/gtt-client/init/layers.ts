bdd465ddcee5e932221aa53e5c287205132e1325
Daniel Kastl <daniel@georepublic.de>
2024-02-26 12:20:54 +0900

    Upgrades to OpenLayers 9
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- src/components/gtt-client/init/layers.ts
+++ src/components/gtt-client/init/layers.ts
@@ -194,7 +194,7 @@ function addVectorLayer(this: any, features: Feature<Geometry>[] | null): void {
 
   this.vector.set('title', 'Features');
   this.vector.set('displayInLayerSwitcher', false);
-  this.vector.on('prerender', () => this.map.flushDeclutterItems());
+  // this.vector.on('prerender', () => this.map.flushDeclutterItems());
 
   // Listen to the moveend event and show message when zoom level is too low
   let previousZoom = this.map.getView().getZoom();
