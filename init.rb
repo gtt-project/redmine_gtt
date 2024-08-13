8dbd8500100b35f306e6f6519c91825ac93b6dcd
Daniel Kastl <daniel@georepublic.de>
2024-08-13 14:21:59 +0200

    Prepares for v5.1.2
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_gtt do
   author_url 'https://github.com/georepublic'
   url 'https://github.com/gtt-project/redmine_gtt'
   description 'Adds location-based task management and maps'
-  version '5.1.0'
+  version '5.1.2'
 
   requires_redmine :version_or_higher => '5.0.0'
 
